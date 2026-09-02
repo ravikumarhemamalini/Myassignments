dc4254253a45a877121ab9cb85c0990f54bf7b2c
ravikumarhemamalini <ravikumarhemamalini@gmail.com>
2026-09-02 08:39:30 -0400

    Add files via upload

---
+++ Week1/Day2/assignDataType.js
@@ -0,0 +1,31 @@
+//*string = username * //
+
+let username = "hema"
+console.log(username);
+console.log(typeof username);
+
+//*string = Organization *//
+
+let organization = "google"
+console.log(organization);
+console.log(typeof organization);
+
+//*number = contactnumber *//
+
+let contactnumber = 8939238439
+console.log(contactnumber);
+console.log(typeof contactnumber);
+
+//*Boolen = knowsAutomation *//
+
+let knowsAutomation = true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+//*undefined = usesPlaywright *//
+
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+
